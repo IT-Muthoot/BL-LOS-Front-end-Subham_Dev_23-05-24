ada8056c5ba03aa44a657edc0706b2b801988ffb
FF63809 <itveena@muthootgroup.com>
2024-10-17 17:28:19 +0530

    Updated from Veena

--- src/app/components/loan-calculator/loan-calculator.component.ts
+++ src/app/components/loan-calculator/loan-calculator.component.ts
@@ -45,7 +45,7 @@ export class LoanCalculatorComponent {
   amortizationType: string = 'Yearly'; 
    // Default value, can be 'Monthly' or 'Yearly'
   formatLabel(value: number): string {
-    if (value >= 1000) {
+    if (value >= 10000) {
       return Math.round(value / 1000) + 'k';
     }
 
